edddc21480598f931c76b1a546ec692dcd057291
fernandogoncalves-me <fernandosg88@gmail.com>
2024-10-15 22:28:50 +0200

    chore: adds TF code

---
+++ infra/cloudfront.tf
@@ -0,0 +1,44 @@
+locals {
+  s3_origin_id = "s3Hosting"
+}
+
+resource "aws_cloudfront_distribution" "blog" {
+  enabled             = true
+  default_root_object = "index.html"
+  price_class         = "PriceClass_200"
+
+  origin {
+    domain_name = aws_s3_bucket_website_configuration.hosting.website_endpoint
+    origin_id   = local.s3_origin_id
+  }
+
+  default_cache_behavior {
+    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+      locations        = []
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
---
+++ infra/versions.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.71"
+    }
+  }
+
+  cloud {
+    organization = "cloudtalents"
+
+    workspaces {
+      name = "tech-blog"
+    }
+  }
+}
